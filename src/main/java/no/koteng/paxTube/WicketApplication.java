777b26c6c5f623732f6a1c6cfcfb5d05f231e2e0
pax2k <raymondkoteng@gmail.com>
2013-06-17 22:03:39 +0200

    Wicket runns in deployment mode

--- src/main/java/no/koteng/paxTube/WicketApplication.java
+++ src/main/java/no/koteng/paxTube/WicketApplication.java
@@ -1,7 +1,13 @@
 package no.koteng.paxTube;
 
+import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.http.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class WicketApplication extends WebApplication {
 
@@ -11,9 +17,12 @@ public class WicketApplication extends WebApplication {
     }
 
     @Override
+    public RuntimeConfigurationType getConfigurationType() {
+        return RuntimeConfigurationType.DEPLOYMENT;
+    }
+
+    @Override
     public void init() {
         super.init();
-
-        getDebugSettings().setAjaxDebugModeEnabled(true);
     }
 }
