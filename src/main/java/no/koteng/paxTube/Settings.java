3fe5c710120c6c652636202a5423f25336e163b0
pax2k <raymondkoteng@gmail.com>
2013-06-19 22:14:09 +0200

    Settings.java is back to git tracking

--- src/main/java/no/koteng/paxTube/Settings.java
+++ src/main/java/no/koteng/paxTube/Settings.java
@@ -1,6 +1,6 @@
 package no.koteng.paxTube;
 
 public class Settings {
-    public static String YOUTUBE_API_KEY = "aLongYouTubeAppKey";
-    public static String YOUTUBE_APPLICATION_NAME = "AppName";
+    public static String YOUTUBE_API_KEY = "";
+    public static String YOUTUBE_APPLICATION_NAME = "";
 }
